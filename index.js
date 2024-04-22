feefbe7b2204acb9d218f022f0a173677d62a471
Ramanujam Gond <ramanujamgond@gmail.com>
2024-04-22 12:37:27 +0530

    feat: develop a small time stamp microservice using nodejs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +21,23 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+// api end points for showing timestamp
+app.get("/api/:date?", (req, res) => {
+  let inputDate = parseInt(req.params.date);
+
+  if (!inputDate) {
+    res.json({ unix: new Date().valueOf(), utc: new Date().toUTCString() });
+  }
+
+  if (inputDate) {
+    res.json({ unitx: inputDate, utc: new Date(inputDate).toUTCString() })
+  } else {
+    res.json({ error: "Invalid Date" })
+  }
+})
 
 
 // Listen on port set in environment variable or default to 3000
